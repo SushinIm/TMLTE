b8331818d21e63283525105e12783977db431122
pc-PC\sushin <tntls5082@gmail.com>
2017-08-10 09:38:50 +0900

    수신 : 티켓 디테일

--- LTE/src/main/java/com/TM/LTE/ReserveController.java
+++ LTE/src/main/java/com/TM/LTE/ReserveController.java
@@ -5,55 +5,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class ReserveController {
+	private ModelAndView mav;   //�슂泥�, ���옣 �걹�궃�썑 �궗�씪�졇�꽌 
 
-   private ModelAndView mav;   //요청, 저장 끝난후 사라져서 
+
+	@RequestMapping(value = "/airInternals", method = RequestMethod.GET)
+	public ModelAndView internalTour() {
+		mav = new ModelAndView();
+		mav.setViewName("internalair");
+		return mav;
+	}
+
+	@RequestMapping(value = "/hotelInternals", method = RequestMethod.GET)
+	public ModelAndView internalHotel() {
+		mav = new ModelAndView();
+		mav.setViewName("internalhotel");
+		return mav;
+	}
+
+	@RequestMapping(value = "/ticketInternals", method = RequestMethod.GET)
+	public ModelAndView internalTicket() {
+		mav = new ModelAndView();
+		mav.setViewName("internalticket");
+		return mav;
+	}
+
+	@RequestMapping(value = "/airOverseas", method = RequestMethod.GET)
+	public ModelAndView toAir() {
+		mav = new ModelAndView();
+		mav.setViewName("overseasair");
+		return mav;
+	}
+
+	@RequestMapping(value = "/hotelOverseas", method = RequestMethod.GET)
+	public ModelAndView toHotel() {
+		mav = new ModelAndView();
+		mav.setViewName("overseashotel");
+		return mav;
+	}
+
+	@RequestMapping(value = "/ticketOverseas", method = RequestMethod.GET)
+	public ModelAndView toTicket() {
+		mav = new ModelAndView();
+		mav.setViewName("overseasticket");
+		return mav;
+	}
 
 
-   @RequestMapping(value = "/airInternals", method = RequestMethod.GET)
-   public ModelAndView internalTour() {
-      mav = new ModelAndView();
-      mav.setViewName("internalair");
-      return mav;
-   }
+	@RequestMapping(value = "/ticketOverseas", method = RequestMethod.GET)
+	public ModelAndView buyTicket() {
+		mav = new ModelAndView();
+		mav.setViewName("overseasticket");
+		return mav;
+	}  
 
-   @RequestMapping(value = "/hotelInternals", method = RequestMethod.GET)
-   public ModelAndView internalHotel() {
-      mav = new ModelAndView();
-      mav.setViewName("internalhotel");
-      return mav;
-   }
-
-   @RequestMapping(value = "/ticketInternals", method = RequestMethod.GET)
-   public ModelAndView internalTicket() {
-      mav = new ModelAndView();
-      mav.setViewName("internalticket");
-      return mav;
-   }
-
-   @RequestMapping(value = "/airOverseas", method = RequestMethod.GET)
-   public ModelAndView toAir() {
-      mav = new ModelAndView();
-      mav.setViewName("overseasair");
-      return mav;
-   }
-
-   @RequestMapping(value = "/hotelOverseas", method = RequestMethod.GET)
-   public ModelAndView toHotel() {
-      mav = new ModelAndView();
-      mav.setViewName("overseashotel");
-      return mav;
-   }
-
-   @RequestMapping(value = "/ticketOverseas", method = RequestMethod.GET)
-   public ModelAndView toTicket() {
-      mav = new ModelAndView();
-      mav.setViewName("overseasticket");
-      return mav;
-   }
-   
 }
---
+++ LTE/src/main/webapp/WEB-INF/views/ticketdetail.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div id="prodimg"></div>
+	<div id="prodinfo">
+		<form>
+			<p></p>
+			<p></p>
+			<p></p>
+			<select>
+				<option></option>
+				<option></option>
+				<option></option>
+			</select> <select>
+				<option></option>
+				<option></option>
+				<option></option>
+			</select>
+			<p>50</p>
+			<input type="submit" />
+		</form>
+	</div>
+	<div id="prodintr"></div>
+	<div id="prodwarn"></div>
+</body>
+</html>

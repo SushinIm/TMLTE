7805c27420893fa0dc9d99023028ec8a3e9c63c2
영호 <syhforce@gmail.com>
2017-08-11 15:59:29 +0900

    조금 수정

--- LTE/src/main/java/com/TM/LTE/dao/MemberDao.java
+++ LTE/src/main/java/com/TM/LTE/dao/MemberDao.java
@@ -1,7 +1,6 @@
 package com.TM.LTE.dao;
 
 import java.util.List;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,8 +9,10 @@ import com.TM.LTE.bean.Member;
 
 @Repository
 public class MemberDao {
+	
 	@Autowired
-	private static SqlSessionTemplate sqlSession;
+	private SqlSessionTemplate sqlSession;
+	
 	public List<Member> adminSeller() {
 		return sqlSession.selectList("Member.adminSeller");
 	}
